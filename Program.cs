c12dc250cb6f74d692e6777f49f0f83a77638b9b
Rodrigo Oliveira <oliveiraroma@gmail.com>
2023-06-05 23:22:42 -0300

    Fix35 V3.1

--- Program.cs
+++ Program.cs
@@ -5,5 +5,7 @@
         Console.WriteLine("Commit V1.0");
         Console.WriteLine("Commit V2.0");
         Console.WriteLine("Commit V3.0");
+        Console.WriteLine("Commit V4.0");
+        Console.WriteLine("Commit V3.1");
     }
 }
